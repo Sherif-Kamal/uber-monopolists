ef24987dbd12c21f36c4c60476eeed46c1f98e8e
Sherif <sh.kamal@ibiidi.co.uk>
2020-09-03 14:04:17 +0200

    added year index

--- src/monopolists/monopolists.schema.ts
+++ src/monopolists/monopolists.schema.ts
@@ -6,7 +6,7 @@ export const MonopolistsSchema = new mongoose.Schema({
 
   driver_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   month: {type:Number,enum: MONTHS},
-  year: {type: Number},
+  year: {type: Number, index : true},
   counter: {type: Number,default: 0},
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date }
